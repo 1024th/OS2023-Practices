2ac7366052fd13f3638436eef2a96c2ea44899b7
1024th <lintianchuan@foxmail.com>
2023-05-07 22:27:17 +0800

    fix cur_co recovery revert the patch that selects parent coroutine when a coroutine finishes

--- practice-1/coroutine/coroutine.c
+++ practice-1/coroutine/coroutine.c
@@ -10,6 +10,14 @@
 
 #include "pthread.h"
 
+#ifdef DEBUG
+#define dbg_printf(...)                                                  \
+  fprintf(stderr, "[thread: %ld, co #%d] ", pthread_self(), co_getid()); \
+  fprintf(stderr, __VA_ARGS__)
+#else
+#define dbg_printf(...)
+#endif
+
 struct node {
   struct node* nxt;
   struct node* pre;
@@ -18,6 +26,18 @@ struct node {
 
 struct coroutine_list {
   struct node* head;
+};
+
+#define STACK_SIZE 65536
+struct coroutine_t {
+  int cid;
+  int (*func)(void);
+  int status;
+  int retval;
+  jmp_buf env;
+  uint8_t* stack;
+  struct coroutine_list waiting_cors;
+  struct coroutine_t* parent;
 };
 
 void add_node(struct coroutine_list* l, struct node* n) {
@@ -53,6 +73,7 @@ void add_all(struct coroutine_list* l1, struct coroutine_list* l2) {
 void add_all_to_array(struct coroutine_t** l1, size_t* size, struct coroutine_list* l2) {
   struct node* n = l2->head;
   while (n != NULL) {
+    dbg_printf("add #%d to avail array\n", n->c->cid);
     add_to_array(l1, size, n->c);
     n = n->nxt;
   }
@@ -81,10 +102,12 @@ void remove_from_list(struct coroutine_list* l, struct node* n) {
 }
 
 void remove_from_array(struct coroutine_t** arr, size_t* size, struct coroutine_t* c) {
+  dbg_printf("remove #%d from avail array\n", c->cid);
   int i = 0;
   while (arr[i] != c && i < *size) {
     i++;
   }
+  assert(i < *size && arr[i] == c);
   while (i + 1 < *size) {
     arr[i] = arr[i + 1];
     i++;
@@ -102,25 +125,13 @@ void remove_all(struct coroutine_list* l) {
   }
 }
 
-#define STACK_SIZE 65536
-struct coroutine_t {
-  int cid;
-  int (*func)(void);
-  int status;
-  int retval;
-  jmp_buf env;
-  uint8_t stack[STACK_SIZE];
-  struct coroutine_list waiting_cors;
-  struct coroutine_t* parent;
-};
-
 // Use a thread local variable to store coroutine manager for each thread.
 __thread struct co_maganer_t {
   int is_initialized;
   struct coroutine_t __cors[MAXN + 10];
   struct coroutine_t* cors;
   struct coroutine_t* main_co;
-  struct coroutine_t* cur_coroutine;
+  struct coroutine_t* cur_co;
   struct coroutine_t* avail_cors[MAXN + 10];
   size_t avail_cor_num;
   struct node cor_nodes[MAXN + 10];
@@ -129,7 +140,7 @@ __thread struct co_maganer_t {
 
 void init_co_manager() {
   co_manager.__cors[0] = (struct coroutine_t){.cid = -1, .func = NULL, .status = RUNNING};
-  co_manager.cur_coroutine = co_manager.main_co = co_manager.__cors;
+  co_manager.cur_co = co_manager.main_co = co_manager.__cors;
   co_manager.cors = co_manager.__cors + 1;
   co_manager.cor_num = 0;
   co_manager.unfinished_cor_num = 0;
@@ -140,34 +151,35 @@ void init_co_manager() {
 
 void select_and_switch();
 
-void continue_coroutine(struct coroutine_t* c) { longjmp(c->env, 1); }
+void continue_coroutine(struct coroutine_t* c) {
+  co_manager.cur_co = c;
+  longjmp(c->env, 1);
+}
 
 void coroutine_finish(int retval) {
-#ifdef DEBUG
-  fprintf(stderr, "co #%d finished with retval %d\n", co_manager.cur_coroutine->cid, retval);
-#endif
-  co_manager.cur_coroutine->retval = retval;
-  co_manager.cur_coroutine->status = FINISHED;
+  dbg_printf("co #%d finished with retval %d\n", co_manager.cur_co->cid, retval);
+  co_manager.cur_co->retval = retval;
+  co_manager.cur_co->status = FINISHED;
   co_manager.unfinished_cor_num--;
-  add_all_to_array(co_manager.avail_cors, &co_manager.avail_cor_num, &co_manager.cur_coroutine->waiting_cors);
-  remove_from_array(co_manager.avail_cors, &co_manager.avail_cor_num, co_manager.cur_coroutine);
-#ifdef DEBUG
-  fprintf(stderr, "co #%d parent: co #%d\n", co_manager.cur_coroutine->cid, co_manager.cur_coroutine->parent->cid);
-#endif
-  if (co_manager.cur_coroutine->parent->status != FINISHED) {
-    continue_coroutine(co_manager.cur_coroutine->parent);
-  } else {
-    select_and_switch();
-  }
+  add_all_to_array(co_manager.avail_cors, &co_manager.avail_cor_num, &co_manager.cur_co->waiting_cors);
+  remove_from_array(co_manager.avail_cors, &co_manager.avail_cor_num, co_manager.cur_co);
+
+  // dbg_printf("co #%d parent: co #%d\n", co_manager.cur_co->cid, co_manager.cur_co->parent->cid);
+  // if (co_manager.cur_co->parent->status != FINISHED) {
+  //   continue_coroutine(co_manager.cur_co->parent);
+  // } else {
+  //   select_and_switch();
+  // }
+  // The code above is buggy, because it does not check whether parent is available,
+  // i.e., is not waiting for some other coroutine.
+  select_and_switch();
 }
 
 void run_coroutine(struct coroutine_t* c) {
-  struct coroutine_t* old_co = co_manager.cur_coroutine;
-#ifdef DEBUG
-  fprintf(stderr, "run co #%d, old co #%d\n", c->cid, old_co->cid);
-#endif
+  struct coroutine_t* old_co = co_manager.cur_co;
+  dbg_printf("run co #%d, old co #%d\n", c->cid, old_co->cid);
   c->status = RUNNING;
-  co_manager.cur_coroutine = c;
+  co_manager.cur_co = c;
 
   if (setjmp(old_co->env) == 0) {
     asm(
@@ -175,19 +187,18 @@ void run_coroutine(struct coroutine_t* c) {
         "movq %0, %%rsp; call *%1; movl %%eax, %%edi; call *%2" ::"c"((uintptr_t)c->stack + STACK_SIZE),
         "d"((uintptr_t)c->func), "b"((uintptr_t)coroutine_finish)
 #else
-        "movl %0, %%esp; call *%1; movl %%eax, %%edi; call *%2" ::"c"((uintptr_t)c->stack + STACK_SIZE),
-        "d"((uintptr_t)c->func), "a"((uintptr_t)coroutine_finish)
+// unimplemented
+#error
 #endif
     );
-//     asm(
-// #if __x86_64__
-//         "movq %0, %%rsp" ::"c"((uintptr_t)c->stack + STACK_SIZE)
-// #else
-// #endif
-//     );
-//     coroutine_finish(c->func());
+    //     asm(
+    // #if __x86_64__
+    //         "movq %0, %%rsp" ::"c"((uintptr_t)c->stack + STACK_SIZE)
+    // #else
+    // #endif
+    //     );
+    //     coroutine_finish(c->func());
   }
-  co_manager.cur_coroutine = old_co;
 }
 
 int co_start_nonblock(int (*routine)(void)) {
@@ -195,8 +206,9 @@ int co_start_nonblock(int (*routine)(void)) {
   c->cid = co_manager.cor_num;
   c->func = routine;
   c->status = NEW;  // TODO
+  c->stack = malloc(STACK_SIZE);
   c->waiting_cors.head = NULL;
-  c->parent = co_manager.cur_coroutine;
+  c->parent = co_manager.cur_co;
   add_to_array(co_manager.avail_cors, &co_manager.avail_cor_num, c);
   co_manager.cor_num++;
   co_manager.unfinished_cor_num++;
@@ -213,15 +225,17 @@ int co_start(int (*routine)(void)) {
   return cid;
 }
 
-int co_getid() { return co_manager.cur_coroutine->cid; }
+int co_getid() { return co_manager.cur_co->cid; }
 
 int is_parent_of(struct coroutine_t* c) {
-  if (co_manager.cur_coroutine == c->parent) return 1;
+  if (co_manager.cur_co == c) return 1;
+  if (co_manager.cur_co == c->parent) return 1;
   if (c->parent != NULL) return is_parent_of(c->parent);
   return 0;
 }
 
 int co_getret(int cid) {
+  dbg_printf("get ret of #%d, status %d\n", cid, co_manager.cors[cid].status);
   int retval;
   retval = co_manager.cors[cid].retval;
   return retval;
@@ -237,13 +251,9 @@ int co_status(int cid) {
 }
 
 void select_and_switch() {
-#ifdef DEBUG
-  fprintf(stderr, "enter select_and_switch\n");
-#endif
+  dbg_printf("enter select_and_switch\n");
   struct coroutine_t* c = co_manager.avail_cors[rand() % co_manager.avail_cor_num];
-#ifdef DEBUG
-  fprintf(stderr, "select coroutine #%d to switch\n", c->cid);
-#endif
+  dbg_printf("select coroutine #%d to switch\n", c->cid);
   if (c->status == NEW)
     run_coroutine(c);
   else
@@ -251,26 +261,25 @@ void select_and_switch() {
 }
 
 int co_yield () {
-  struct coroutine_t* c = co_manager.cur_coroutine;
+  struct coroutine_t* c = co_manager.cur_co;
   int x = setjmp(c->env);
   if (x == 0) {
     select_and_switch();
-  } else {
-    co_manager.cur_coroutine = c;
   }
   return 0;
 }
 
 int co_waitall() {
   while (co_manager.unfinished_cor_num) {
-    co_yield();
+    co_yield ();
   }
 }
 
 int co_wait(int cid) {
+  dbg_printf("co #%d wait for co #%d\n", co_manager.cur_co->cid, cid);
   struct coroutine_t *cur, *c;
   int need_wait = 0;
-  cur = co_manager.cur_coroutine;
+  cur = co_manager.cur_co;
   c = &co_manager.cors[cid];
   if (c->status != FINISHED) {
     add(&c->waiting_cors, cur);
@@ -283,7 +292,5 @@ int co_wait(int cid) {
   int x = setjmp(cur->env);
   if (x == 0) {
     select_and_switch();
-  } else {
-    co_manager.cur_coroutine = cur;
   }
 }
